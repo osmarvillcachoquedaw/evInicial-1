7edf3894ad4f927ad2eb20bebda0dd4959429280
osmar villca <osmarvillcachoquedaw@gmail.com>
2016-02-29 00:15:01 +0100

    corregirNumerica hecho

--- api/controllers/RespuestaController.js
+++ api/controllers/RespuestaController.js
@@ -21,15 +21,16 @@ module.exports = {
 	//paso 2 -->carga pregunta
 	//paso 3 -->respuesta.Controller envia (idOpcion,idUsuario,idCuest, idPreg) a pregunta.comprobarRespuesta
 	corregirRespuesta: function(req, res, next){
-		sails.log.verbose("1 "+req.body.answered);
+		/*sails.log.verbose("1 "+req.body.answered);
 		sails.log.verbose("2 "+req.session.passport.user);
 		sails.log.verbose("3 "+req.cuestionario.id);
-		sails.log.verbose("4 "+req.pregunta.id);
-		var tipoPregunta=req.pregunta;
+		sails.log.verbose("4 "+req.pregunta.id);*/
+		var pregunta=req.pregunta;
 		//sails.log.verbose("5 "+pregun.tipo);
-		req.pregunta.tipoPregunta(req.body.answered, req.session.passport.user, req.cuestionario.id,  tipoPregunta, function ahi(funcion){res.json(funcion);});
-	}
-
-
+ 		req.pregunta.tipoPregunta(req.body.answered, req.session.passport.user, req.cuestionario.id, pregunta, function (created){
+ 			res.json(created);
+ 		});
+  	}
+	
 };
 
--- api/models/Pregunta.js
+++ api/models/Pregunta.js
@@ -35,52 +35,37 @@ module.exports = {
     },
 
     /*Adaptando tipo preguntas y dependiendo el tipo de pregunta que sea, se asigna una funcion*/
-    tipoPregunta:function(respuesta, user, cuestionario, tipoPregunta, cb){
-        switch (tipoPregunta.tipo) {
+    tipoPregunta:function(respuesta, user, cuestionario, pregunta, cb){
+        switch (pregunta.tipo) {
           case "Ensayo":
-                console.log("Pregunta tipo: "+tipoPregunta.id);
+                console.log("Pregunta tipo: "+pregunta.id);
                 break;
           case "Numerica": 
-                console.log("Pregunta tipo: "+tipoPregunta.id);
-                this.comprobarNumerica(respuesta, function (valortext, valorfraction, cb){
-                    Alumno.findOne({user: user})
-                    .then(function(alumnum){
-                        if(alumnum){
-                            console.log("alumnum"+alumnum);
-                        Respuesta.create({valor: valortext, puntuacion: valorfraction, alumno: alumnum.id, cuestionario:cuestionario , pregunta: tipoPregunta.id})
-                        .exec(function funcion(err, funcion){
-                        cb(funcion);
-                        });
-                        }else{
-                            sails.log.verbose("No estas autenticado como usuario Alumno");
-                        }
+                console.log("Pregunta tipo: "+pregunta.id);
+                this.comprobarNumerica(respuesta, function (valorfraction,valortext){
+                     Alumno.findOne({user: user})
+                     .then(function(alumnum){
+                         if(alumnum){
+                         Respuesta.create({valor: valortext, puntuacion: valorfraction, alumno: alumnum.id, cuestionario:cuestionario , pregunta: pregunta.id})
+                         .exec(function funcion(err, funcion){
+                         cb(funcion);
+                         });
+                         }else{
+                             sails.log.verbose("No estas autenticado como usuario Alumno");
+                         }
                     })
                 })
                 break;
           case "Emparejamiento":
-                console.log("Pregunta tipo: "+tipoPregunta.id);
+                console.log("Pregunta tipo: "+pregunta.id);
                 statements // they are executed if variable ==  any of the above c's
                 break;
           case "Verdadero/Falso":  
-                console.log("Pregunta tipo: "+tipoPregunta.id);      
+                console.log("Pregunta tipo: "+pregunta.id);      
                 statements // they are executed if variable ==  any of the above c's
                 break;
           case "Eleccion multiple": 
-                console.log("Pregunta tipo: "+tipoPregunta.id);       
-                this.comprobarEleccionMultiple(respuesta, function cb(){
-                    Alumno.findOne({
-                        where: {user: user}
-                    }).then(function(alumno){
-                        if(alumno){
-                            Respuesta.create({valor: "Correcto", puntuacion: 100, cuestionario: cuestionario, pregunta: pregunta, alumno: alumno.id})
-                            .exec(function createCB(err, created){
-                                res.json(created);
-                            })
-                        }else{
-                            sails.log.verbose("No estas autenticado como usuario Alumno");
-                        }
-                    })
-                });
+                
                 break;    
           /*default:
                 statements // they are executed if none of the above case is satisfied
@@ -92,48 +77,33 @@ module.exports = {
 
     //para -.>NUMERICA
     comprobarNumerica: function(respuesta, cb){
-        console.log("Entrando a comprobarNumerica");
-        var idRespuesta = respuesta;//id de la opcion que envia el usuario 
-        var valortext;
-        var valorfraction;
-        Opcion.findOne({
-            where: { id: Number(idRespuesta)}
-          }).populate('subopciones').then(function(opcionSeleccionada){
-                opcionSeleccionada.subopciones.forEach(function(unasubopcion){
-                    if(unasubopcion.nombre == "fraction"){
-                        valorfraction = unasubopcion.valor;
-                        //console.log(""+uno);
-                        sails.log.verbose(valorfraction);
-                    }
-                    if(unasubopcion.nombre == "text"){
-                        valortext = unasubopcion.valor;
-                        //console.log("dos"+dos);
-                        sails.log.verbose(valortext);
-                    }  
-                    cb(valorfraction, valortext);                
-                });
-                
-        })
+         var idRespuesta = respuesta;//id de la opcion que envia el usuario 
+         var valortext;
+         var valorfraction;
+         Opcion.findOne({
+             where: { id: Number(idRespuesta)}
+           }).populate('subopciones').then(function(opcionSeleccionada){
+                 opcionSeleccionada.subopciones.forEach(function(unasubopcion){
+                     if(unasubopcion.nombre == "fraction"){
+                         valorfraction = unasubopcion.valor;
+                         //console.log(""+uno);
+                         sails.log.verbose(valorfraction);
+                     }
+                     if(unasubopcion.nombre == "text"){
+                         valortext = unasubopcion.valor;
+                         //console.log("dos"+dos);
+                         sails.log.verbose(valortext);
+                     }                  
+                 });
+                 cb(valorfraction, valortext);    
+         })
     },
     //para -.>EMPAREJAMIENTO
     
     //para -.>VERDADERO/FALSO
     
     //para -->ELECCION MULTIPLE
-    comprobarEleccionMultiple: function(respuesta, cb){
-        Subopcion.findOne({
-            where: {opcion: Number(respuesta), nombre: "fraccion"}
-        }).then(function(subopcion){
-            var puntuacion = subopcion.valor;
-            Subopcion.findOne({
-                where: {opcion: Number(respuesta), nombre: "text"}
-            }).then(function(subopcion){
-                var texto = subopcion.valor;
-                cb(puntuacion, texto);
-            })  
-        })
-    },
-
+    
 
   }
 };
